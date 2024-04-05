7e3c08479715ea2d9c8baa849066fcfb5ae53382
JangSeongGyu <jang.seongyu@mic-p.com>
2024-04-05 10:55:55 +0900

    CleanUp

--- backend/resources/js/route.jsx
+++ backend/resources/js/route.jsx
@@ -1,47 +1,11 @@
 import React from 'react';
 import Home from './Home';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import { ThemeProvider } from '@mui/material';
-import Theme from './Theme';
-import Login from './Login';
-import LoginError from './LoginError';
-import Main from './Main';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 const Router = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route
-                    path={`/login/:table_id`}
-                    element={
-                        <ThemeProvider theme={Theme}>
-                            <Login />
-                        </ThemeProvider>
-                    }
-                />
-                <Route
-                    path={`/game`}
-                    element={
-                        <ThemeProvider theme={Theme}>
-                            <Home />
-                        </ThemeProvider>
-                    }
-                />
-                <Route
-                    path={`/loginerror`}
-                    element={
-                        <ThemeProvider theme={Theme}>
-                            <LoginError />
-                        </ThemeProvider>
-                    }
-                />{' '}
-                <Route
-                    path={`/main/:task`}
-                    element={
-                        <ThemeProvider theme={Theme}>
-                            <Main />
-                        </ThemeProvider>
-                    }
-                />
+                <Route path={`/home`} element={<Home />} />
             </Routes>
         </BrowserRouter>
     );
